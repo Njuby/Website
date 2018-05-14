2f59decb776be83f2fcdef39478f75ba7255f23c
David_Elout <dedavid.elout@gmail.com>
2018-05-14 14:04:56 +0200

    Removed footer, for now

--- BootstrapWebsite/mail/contact_me.php
+++ BootstrapWebsite/mail/contact_me.php
@@ -16,10 +16,10 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'eloutcreations.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com. Phone: $phone\n\n
+$to = 'david@eloutcreations.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "New message from:  $name";
+$email_body = "Name: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@eloutcreations.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com. Phone: $phone\n\n
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
