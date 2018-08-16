8777574d11b5401f626cfeaa24799a902f032216
David_Elout <dedavid.elout@gmail.com>
2018-08-16 15:10:01 +0200

    mostly working on the detailed game opages

--- BootstrapWebsite/mail/contact_me.php
+++ BootstrapWebsite/mail/contact_me.php
@@ -19,7 +19,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'david@eloutcreations.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "New message from:  $name";
 $email_body = "Name: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@eloutcreations.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com. Phone: $phone\n\n
+$headers = "From: noreply@eloutcreations.com\n"; // This is the email address the generated message will be from. 
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
